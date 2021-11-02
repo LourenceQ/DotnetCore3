af167a3f84c8cef758935d52ef1166b1110f64ba
LourenceQ <lqfmecatronica@gmail.com>
2021-11-02 11:29:25 -0300

    Adicionando classe ServiceResponse para informar dados sobre cada requisição do cliente

--- DotnetCore3/Services/CharacterService/ICharacterService.cs
+++ DotnetCore3/Services/CharacterService/ICharacterService.cs
@@ -6,8 +6,8 @@ namespace DotnetCore3.Services.CharacterService
 {
     public interface ICharacterService
     {
-        Task<List<Character>> GetAllCharacters();
-        Task<Character> GetCharacterById(int id);
-        Task<List<Character>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
     }
 }
