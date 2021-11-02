8718b5d041001b05921586ecf608b8cc1a325ee9
LourenceQ <lqfmecatronica@gmail.com>
2021-11-02 10:46:40 -0300

    declaração de classe de implementaçao CharacterService

--- DotnetCore3/Startup.cs
+++ DotnetCore3/Startup.cs
@@ -1,3 +1,4 @@
+using DotnetCore3.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace DotnetCore3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICharacterService, CharacterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
