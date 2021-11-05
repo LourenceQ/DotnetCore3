d1d8ac4f3f4578238182da20bd8bceac45f3c0fa
LourenceQ <lqfmecatronica@gmail.com>
2021-11-05 16:12:22 -0300

    Inserindo string de conexão com banco de dados

--- DotnetCore3/Startup.cs
+++ DotnetCore3/Startup.cs
@@ -1,8 +1,10 @@
+using DotnetCore3.Data;
 using DotnetCore3.Services.CharacterService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace DotnetCore3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
