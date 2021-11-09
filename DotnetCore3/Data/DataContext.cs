f3b35189b6a2a47bf1d818558e48ce67b00a5d5c
LourenceQ <lqfmecatronica@gmail.com>
2021-11-08 23:20:03 -0300

    Model usuário

--- DotnetCore3/Data/DataContext.cs
+++ DotnetCore3/Data/DataContext.cs
@@ -6,7 +6,7 @@ namespace DotnetCore3.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
         public DbSet<Character> Characters { get; set; }
+        public DbSet<User> users { get; set; }
     }
 }
---
+++ DotnetCore3/Migrations/20211109021923_User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetCore3.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
