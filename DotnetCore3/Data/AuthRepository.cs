b3a6d255a428ba7959e792330307c3fe8234c73c
LourenceQ <lqfmecatronica@gmail.com>
2021-11-09 23:21:49 -0300

    método para criptografar

--- DotnetCore3/Data/AuthRepository.cs
+++ DotnetCore3/Data/AuthRepository.cs
@@ -29,5 +29,14 @@ namespace DotnetCore3.Data
         {
             throw new System.NotImplementedException();
         }
+
+        private void CreatePassWordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
     }
 }
