527297d284131f27650a14939fcd2c8f5bdb8bb9
LourenceQ <lqfmecatronica@gmail.com>
2021-11-09 23:15:34 -0300

    método Register

--- DotnetCore3/Data/AuthRepository.cs
+++ DotnetCore3/Data/AuthRepository.cs
@@ -5,15 +5,25 @@ namespace DotnetCore3.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string pawssword)
+        public async Task<ServiceResponse<int>> Register(User user, string pawssword)
         {
-            throw new System.NotImplementedException();
-        }
+            await _context.users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;
+         }
 
         public Task<bool> userExists(string username)
         {
