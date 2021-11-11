d3b3250a23ffa57a156bc9e8a2ef61be609c8294
LourenceQ <lqfmecatronica@gmail.com>
2021-11-10 23:46:29 -0300

    regisrando AuthRepository em startup

--- DotnetCore3/Startup.cs
+++ DotnetCore3/Startup.cs
@@ -32,6 +32,7 @@ namespace DotnetCore3
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
             services.AddScoped<ICharacterService, CharacterService>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
