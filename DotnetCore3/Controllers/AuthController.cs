a0efd0ad754a1e983c3bd4e0b8211423a6b75593
LourenceQ <lqfmecatronica@gmail.com>
2021-11-10 23:44:02 -0300

    Dto de registro de usuário & controller Auth

---
+++ DotnetCore3/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using DotnetCore3.Data;
+using DotnetCore3.Dto.User;
+using DotnetCore3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCore3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegisterDto request)
+        {
+            ServiceResponse<int> response = await _authRepository.Register(
+                new User{ Username = request.Username}, request.Password
+            );
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
---
+++ DotnetCore3/Dto/User/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace DotnetCore3.Dto.User
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
