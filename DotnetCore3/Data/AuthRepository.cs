1215e8b16fad412488a72926658bb45a31c2c55a
LourenceQ <lqfmecatronica@gmail.com>
2021-11-12 16:49:42 -0300

    método verificação de senha com System.Security.HMACSHA512

--- DotnetCore3/Data/AuthRepository.cs
+++ DotnetCore3/Data/AuthRepository.cs
@@ -56,5 +56,21 @@ namespace DotnetCore3.Data
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i=0; i<computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
