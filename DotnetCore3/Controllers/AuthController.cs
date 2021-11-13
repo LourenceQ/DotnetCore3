5478feccb47dce1cbfdb0755bcdb47a70b473fc0
LourenceQ <lqfmecatronica@gmail.com>
2021-11-13 18:33:30 -0300

    Criando novo DTO com duas propriedades. Criando método Login em AuthController

--- DotnetCore3/Controllers/AuthController.cs
+++ DotnetCore3/Controllers/AuthController.cs
@@ -28,5 +28,18 @@ namespace DotnetCore3.Controllers
             }
             return Ok(response);
         }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto request)
+        {
+            ServiceResponse<string> response = await _authRepository.Login(
+                request.Username, request.Password
+            );
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
