e7077eb19227cbba068eaa71aa361043861a70e6
LourenceQ <lqfmecatronica@gmail.com>
2021-11-15 09:37:07 -0300

    instalando NuGetPackages. Criando método CreateToken em AuthRepository

--- DotnetCore3/Data/AuthRepository.cs
+++ DotnetCore3/Data/AuthRepository.cs
@@ -1,14 +1,21 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper.Configuration;
 using DotnetCore3.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotnetCore3.Data
 {
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
-        public AuthRepository(DataContext context)
+        private readonly IConfiguration _configuration;
+        public AuthRepository(DataContext context, IConfiguration configuration)
         {
+            _configuration = configuration;
             _context = context;
 
         }
@@ -16,19 +23,19 @@ namespace DotnetCore3.Data
         {
             ServiceResponse<string> response = new ServiceResponse<string>();
             User user = await _context.users.FirstOrDefaultAsync(x => x.Username.ToLower().Equals(username.ToLower()));
-            if(user == null)
+            if (user == null)
             {
                 response.Success = false;
                 response.Message = "User not found.";
             }
             else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-            {                
+            {
                 response.Success = false;
                 response.Message = "Wrong Password";
             }
             else
             {
-                response.Data = user.Id.ToString();
+                response.Data = CreateToken(user);
             }
 
             return response;
@@ -37,18 +44,18 @@ namespace DotnetCore3.Data
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
             ServiceResponse<int> response = new ServiceResponse<int>();
-            if(await UserExists(user.Username))
+            if (await UserExists(user.Username))
             {
                 response.Success = false;
                 response.Message = "Usuário já cadastrado";
                 return response;
             }
-            
+
             CreatePassWordHash(password, out byte[] passwordhash, out byte[] passwordSalt);
 
             user.PasswordHash = passwordhash;
             user.PasswordSalt = passwordSalt;
-            
+
             await _context.users.AddAsync(user);
             await _context.SaveChangesAsync();
             response.Data = user.Id;
@@ -57,17 +64,17 @@ namespace DotnetCore3.Data
 
         public async Task<bool> UserExists(string username)
         {
-            if(await _context.users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
+            if (await _context.users.AnyAsync(x => x.Username.ToLower() == username.ToLower()))
             {
                 return true;
             }
             return false;
-            
+
         }
 
         private void CreatePassWordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
@@ -76,18 +83,34 @@ namespace DotnetCore3.Data
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-                for(int i=0; i<computedHash.Length; i++)
+                for (int i = 0; i < computedHash.Length; i++)
                 {
-                    if(computedHash[i] != passwordHash[i])
+                    if (computedHash[i] != passwordHash[i])
                     {
                         return false;
                     }
                 }
                 return true;
             }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.Tostring()),
+                new Claim(ClaimTyeps.Name, user.UserName)
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value)
+            );
+
+            SigningCredentials cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); 
+            return string.Empty;
         }
     }
 }
