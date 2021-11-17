c8f4921184a12db64c25c9f89a54fabbee846d46
LourenceQ <lqfmecatronica@gmail.com>
2021-11-17 15:17:30 -0300

    método GetById com segunda condição para verificar Id do usuário em questão

--- DotnetCore3/Services/CharacterService/CharacterService.cs
+++ DotnetCore3/Services/CharacterService/CharacterService.cs
@@ -71,7 +71,8 @@ namespace DotnetCore3.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             ServiceResponse<GetCharacterDto> serviceResponse = new ServiceResponse<GetCharacterDto>();
-            Character dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            Character dbCharacter = await _context.Characters
+            .FirstOrDefaultAsync(c => c.Id == id && c.Users.Id == GetUserId());
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
             return serviceResponse;
         }
