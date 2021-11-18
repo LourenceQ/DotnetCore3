1b421232ca2762b367793359e17b260826bc38b3
LourenceQ <lqfmecatronica@gmail.com>
2021-11-18 15:14:04 -0300

    novo Model Weapon. Adicionando migraçoes de relacionamento entre as classes Weapon e Character

--- DotnetCore3/Migrations/20211118181113_Weapon.Designer.cs
+++ DotnetCore3/Migrations/20211118181113_Weapon.Designer.cs
@@ -4,14 +4,16 @@ using DotnetCore3.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotnetCore3.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211118181113_Weapon")]
+    partial class Weapon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
