282b16b27cf6920d0af701dac47b7bb68f0c1ccb
LourenceQ <lqfmecatronica@gmail.com>
2021-11-19 23:00:57 -0300

    Implementando contrller CharacterSkill

---
+++ DotnetCore3/Controllers/CharacterSkillController.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using DotnetCore3.Dto.CharacterSkill;
+using DotnetCore3.Services.CharacterSkillService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCore3.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterSkillController : ControllerBase
+    {
+        private readonly ICharacterSkillService _characterSkillService;
+        public CharacterSkillController(ICharacterSkillService characterSkillService)
+        {
+            _characterSkillService = characterSkillService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill)
+        {
+            return Ok(await _characterSkillService.AddCharacterSkill(newCharacterSkill));
+        }
+    }
+}
