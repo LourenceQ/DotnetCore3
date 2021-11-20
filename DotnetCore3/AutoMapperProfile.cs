3cb19e58c44a733818649a5068c234ac2f28dc08
LourenceQ <lqfmecatronica@gmail.com>
2021-11-20 14:38:25 -0300

    Mapeando skill -> getskillDTO

--- DotnetCore3/AutoMapperProfile.cs
+++ DotnetCore3/AutoMapperProfile.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using AutoMapper;
 using DotnetCore3.Dto.Character;
+using DotnetCore3.Dto.Skill;
 using DotnetCore3.Dto.Weapon;
 using DotnetCore3.Models;
 
@@ -9,9 +11,11 @@ namespace DotnetCore3
     {
         public AutoMapperProfile()
         {
-            CreateMap<Character, GetCharacterDto>();
+            CreateMap<Character, GetCharacterDto>()
+                .ForMember(dto => dto.Skills, c => c.MapFrom(c => c.CharacterSkills.Select(cs => cs.Skill)));
             CreateMap<AddCharacterDto, Character>();
             CreateMap<Weapon, GetWeaponDto>();
+            CreateMap<Skill, GetSkillDto>();
         }
     }
 }
