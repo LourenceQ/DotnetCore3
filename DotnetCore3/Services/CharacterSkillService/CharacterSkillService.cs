1920f525f8b572dcd6f6987534fb3a7af0bff034
LourenceQ <lqfmecatronica@gmail.com>
2021-11-19 22:43:24 -0300

    Incluindo todos os dados da tabela Skills e todos os dados da tabela Weapon relacionados ao Character

--- DotnetCore3/Services/CharacterSkillService/CharacterSkillService.cs
+++ DotnetCore3/Services/CharacterSkillService/CharacterSkillService.cs
@@ -29,8 +29,37 @@ namespace DotnetCore3.Services.CharacterSkillService
             try
             {
                 Character character = await _context.Characters
+                    .Include(c => c.Weapon)
+                    .Include(c => c.CharacterSkills).ThenInclude(cs => cs.Skill)
                     .FirstOrDefaultAsync(c => c.Id == newCharacterSkill.CharacterId &&
                     c.Users.Id == int.Parse(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier)));
+                if(character == null)
+                {
+                    response.Success = false;
+                    response.Message = "Character nto found";
+                    return response;
+                }
+
+                Skill skill = await _context.Skills
+                    .FirstOrDefaultAsync(s => s.Id == newCharacterSkill.SkillId);
+                
+                if(skill == null)
+                {
+                    response.Success = false;
+                    response.Message = "Skill not found";
+                    return response;
+                }
+
+                CharacterSkill characterSkill = new CharacterSkill
+                {
+                     Character = character,
+                     Skill = skill
+                };
+
+                await _context.CharacterSkills.AddAsync(characterSkill);
+                await _context.SaveChangesAsync();
+                
+                response.Data = _mapper.Map<GetCharacterDto>(character);
             }
             catch(Exception ex)
             {
