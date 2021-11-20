98076927a6b17ec5cf72ab9791520bd520802c92
LourenceQ <lqfmecatronica@gmail.com>
2021-11-20 15:11:13 -0300

    Criacaçao de Dto's

---
+++ DotnetCore3/Dto/Fight/AttackResultDto.cs
@@ -0,0 +1,11 @@
+namespace DotnetCore3.Dto.Fight
+{
+    public class AttackResultDto
+    {
+        public string Attacker { get; set; }
+        public string Opponent { get; set; }
+        public int AttackerHP { get; set; }
+        public int OpponentHP { get; set; }
+        public int Damage { get; set; }
+    }
+}
---
+++ DotnetCore3/Dto/Fight/WeaponAttackDto.cs
@@ -0,0 +1,8 @@
+namespace DotnetCore3.Dto.Fight
+{
+    public class WeaponAttackDto
+    {
+        public int AttackerId { get; set; }
+        public int OpponentId { get; set; }
+    }
+}
