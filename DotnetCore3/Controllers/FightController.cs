44c11d0ee93bb9b78a13df4133c1d0a6d0d3bcfc
LourenceQ <lqfmecatronica@gmail.com>
2021-11-21 17:00:59 -0300

    Implementando controller com FightDto e removendo route para tornar controller post padrão

--- DotnetCore3/Controllers/FightController.cs
+++ DotnetCore3/Controllers/FightController.cs
@@ -21,10 +21,17 @@ namespace DotnetCore3.Controllers
         {
             return Ok(await _fightService.WeaponAttack(request));
         }
+
         [HttpPost("Skill")]
         public async Task<IActionResult> SkillAttack(SkillAttackDto request)
         {
             return Ok(await _fightService.SkillAttack(request));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Fight(FightRequestDto request)
+        {
+            return Ok(await _fightService.Fight(request));
         }
     }
 }
