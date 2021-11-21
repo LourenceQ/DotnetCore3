0b02bac9c256f216c60ae434f865d26f9dfb7215
LourenceQ <lqfmecatronica@gmail.com>
2021-11-21 16:56:14 -0300

    Implementando método em interface de FightService para retornar dto de FightResult e pegando FightRequestDto como requisição

---
+++ DotnetCore3/Dto/Fight/FightRequestDto.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace DotnetCore3.Dto.Fight
+{
+    public class FightRequestDto
+    {
+        public List<int> CharacterIds { get; set; }
+    }
+}
---
+++ DotnetCore3/Dto/Fight/FightResultDto.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace DotnetCore3.Dto.Fight
+{
+    public class FightResultDto
+    {
+        public List<string> Log { get; set; } = new List<string>();
+    }
+}
