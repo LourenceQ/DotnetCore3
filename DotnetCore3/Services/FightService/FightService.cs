83088b27bdef3283e339841d00bad6086feb25ed
LourenceQ <lqfmecatronica@gmail.com>
2021-11-20 22:18:33 -0300

    Finalzando FightService.

--- DotnetCore3/Services/FightService/FightService.cs
+++ DotnetCore3/Services/FightService/FightService.cs
@@ -26,6 +26,25 @@ namespace DotnetCore3.Services.FightService
                     .FirstOrDefaultAsync(c => c.Id == request.AttackerId);
                 Character opponent = await _context.Characters.
                     FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+
+                int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strength));
+                damage -= new Random().Next(opponent.Defense);
+                if(damage > 0)
+                    opponent.HitPoints -= damage;
+                if(opponent.HitPoints <= 0)
+                    response.Message = $"{opponent.Name} has been defeated!";
+
+                _context.Characters.Update(opponent);
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.HitPoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };
             }
             catch(Exception ex)
             {
