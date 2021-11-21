d922e89b0cc14530358d75f316b1ca95d35ed9ea
LourenceQ <lqfmecatronica@gmail.com>
2021-11-21 16:56:20 -0300

    Implementando método em interface de FightService para retornar dto de FightResult e pegando FightRequestDto como requisição

--- DotnetCore3/Services/FightService/IFightService.cs
+++ DotnetCore3/Services/FightService/IFightService.cs
@@ -8,5 +8,6 @@ namespace DotnetCore3.Services.FightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
         Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+        Task<ServiceResponse<FightResultDto>> Fight (FightRequestDto request);
     }
 }
